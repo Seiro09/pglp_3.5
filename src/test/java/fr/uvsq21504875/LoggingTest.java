5f9822b6419518056d1b5e8b1b4ab59d40f86cd9
Elarif Hamidou <elarif.hamidou@ens.uvsq.fr>
2020-02-26 22:15:13 +0100

    Création fichiers de test

---
+++ src/test/java/fr/uvsq21504875/LoggingTest.java
@@ -0,0 +1,12 @@
+package fr.uvsq21504875;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoggingTest {
+  @Test
+  public void afficheMessage() throws Exception {
+  }
+
+}
---
+++ src/test/java/fr/uvsq21504875/UneClasseMetierTest.java
@@ -0,0 +1,12 @@
+package fr.uvsq21504875;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UneClasseMetierTest {
+  @Test
+  public void uneMethodeMetier() throws Exception {
+  }
+
+}
