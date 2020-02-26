20464a9035c6f05c0f3f406b7e8486f365248b9a
Elarif Hamidou <elarif.hamidou@ens.uvsq.fr>
2020-02-26 21:42:35 +0100

    Initial commit

---
+++ src/main/java/fr/uvsq21504875/UneClasseMetier.java
@@ -0,0 +1,20 @@
+package fr.uvsq21504875;
+
+import java.time.LocalDateTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class UneClasseMetier
+{
+
+  public void uneMethodeMetier(){
+    System.out.println(LocalDateTime.now()+": Début de uneMethodeMetier");
+
+    //Traitement métiers
+
+    System.out.println(LocalDateTime.now()+": Fin de uneMethodeMetier");
+
+  }
+}
